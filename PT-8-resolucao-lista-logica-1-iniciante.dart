cec8b08d3841c01292d87393c3ed8590432b0355
Vitor Fantin <fantinfx@gmail.com>
2025-07-02 20:08:31 -0300

    finalização dos exercicios, indenação e exclusao grupo 10 de exercicios nao faz sentido

--- PT-8-resolucao-lista-logica-1-iniciante.dart
+++ PT-8-resolucao-lista-logica-1-iniciante.dart
@@ -1,8 +1,79 @@
+main() {
+  // ### 8. Laços com Listas
 
+  // 1. **Imprima todos os itens da lista**
+  //    Explicação: Dada uma lista de nomes, imprima cada um com `for`.
+  //    Dica: Use `for (var item in lista)`.
+  print("-------- RESOLUCAO ITEM 1 -------");
+  List<String> listaNomes = [
+    "Vitor",
+    "Alexandre",
+    "Maria",
+    "José",
+    "Luciana",
+    "Renata",
+  ];
+  print("Abaixo laço for para todos os nomes da lista !");
+  for (String nomes in listaNomes) {
+    print(nomes);
+  }
 
-void main() {
+  // 2. **Some todos os números da lista**
+  //    Explicação: Calcule soma dos números de uma lista.
+  //    Dica: Use variável acumuladora.
+  print("-------- RESOLUCAO ITEM 2 -------");
+  List <int> listaNumeros = [1,20,30,44,11,23,41,54,77,54];
+  int resultadoLista = 0;
+  for (int somaLista in listaNumeros) {
+    resultadoLista += somaLista;
+     
+  }
+  print("Soma da lista percorrida é de: $resultadoLista");
+
+  // 3. **Conte quantos números são pares**
+  //    Explicação: Conte elementos pares em uma lista.
+  //    Dica: Use `%` dentro do laço.
+   print("-------- RESOLUCAO ITEM 3 -------");
+  List <int> listaNum = [1,3,5,6,2,4,8,10,12,22,31,15,55];
+  List <int> listaPares = [];
+  for (int numero in listaNum) {
+    if (numero % 2 == 0) {
+      listaPares.add(numero);
+      
+    }
+   
+  }
+  print("Os números somente Pares da lista geral: $listaPares");
+ 
+  // 4. **Crie nova lista com números dobrados**
+  //    Explicação: Dada uma lista, crie outra com valores multiplicados por 2.
+  //    Dica: Use laço para popular nova lista.
+  print("-------- RESOLUCAO ITEM 4 -------");
+
+  List <int> listaNumerosNovamente = [2,4,9,5,23,40,56,54,89];
+  List <int> listaNumDobrados = [];
+  int numDobrado = 0;
+  for (int numero in listaNumerosNovamente) {
+    numDobrado = numero * 2;
+    listaNumDobrados.add(numDobrado);    
+  }
+  print("Lista dos Numero dobrados: $listaNumDobrados");
+
+  // 5. **Remova números pares da lista original**
+  //    Explicação: Remova todos os números pares da lista.
   
+  print("-------- RESOLUCAO ITEM 5 --------");
+  
+  List <int> listaNumber = [3,9,5,6,33,88,65,21,43,66,98,76,47,10,20,30,50,2,4,6,8];
+  List <int> listaImpar = [];
+  print("Lista Original $listaNumber");
 
-
-
+  for (int numero in listaNumber) {
+    if (numero % 2 != 0) {
+      listaImpar.add(numero);
+      
+    }
+    
+  }
+  print("Lista somente impares $listaImpar");
 }
--- PT-9-resolucao-lista-logica-1-iniciante.dart
+++ PT-9-resolucao-lista-logica-1-iniciante.dart
@@ -1,8 +1,189 @@
 
 
+import 'dart:io';
+
 void main() {
   
+// ### 9. Switch / Case
 
+// 1. **Nome do dia da semana**  
+//    Explicação: Use número de 1 a 7 para mostrar nome do dia.  
+//    Dica: Use `switch` e `case`.
+
+/* RESOLUCAO ITEM 1
+print("Escolhe qual dia da semana > 1-Segunda | 2-Terça-feira | 3-Quarta-feira | 4-Quinta-feira | 5-Sexta-feira | 6- Sábado | 7-Domingo");
+int  entrada = int.parse(stdin.readLineSync()!);
+
+switch(entrada){
+  case 1:
+  print("Você escolheu Segunda-feira !");
+  break;
+  case 2:
+  print("Você escolheu Terça-feira !");
+  break;
+  case 3:
+  print("Você escolheu Quarta-feira !");
+  break;
+  case 4:
+  print("Você escolheu Quinta-feira !");
+  break;
+  case 5:
+  print("Você escolheu Sexta-feira !");
+  break;
+  case 6:
+  print("Você escolheu Sábado !");
+  break;
+  case 7:
+  print("Você escolheu Domingo !");
+  break;
+  
+}
+*/
+
+// 2. **Classifique uma fruta pelo código**  
+//    Explicação: Use código para mostrar nome da fruta.  
+//    Dica: Use `switch`.
+/* RESOLUCAO ITEM 2
+  print("Digite código de 1 ao 5 e descubra qual fruta que é: ");
+  int entradadois = int.parse(stdin.readLineSync()!);
+  switch (entradadois) {
+    case 1:
+    print("Maça");
+      
+      break;
+      case 2: 
+      print("Uva");
+      break;
+      case 3:
+      print("Laranja");
+      break;
+      case 4: 
+      print("Pêra");
+      break;
+      case 5:
+      print("Banana");
+      break;
+    
+  }
+ */
+
+// 3. **Verifique operador aritmético**  
+//    Explicação: Peça operador e mostre qual foi escolhido.  
+//    Dica: `switch` com `String`.
+
+/* RESOLUCAO ITEM 3
+
+print("Digite  operador matematico que desseja que é: ");
+  String entrada = stdin.readLineSync()!;
+    switch (entrada)
+    {case "+":
+    print("Voce escolhe operador +");
+      
+      break;
+      case "-": 
+       print("Voce escolhe operador -");
+      break;
+      case "*":    print("Voce escolhe operador *");
+      break;
+      case "/": 
+      print("Voce escolhe operador /");
+      break;
+};
+
+*/
+    
+
+// 4. **Calculadora simples com switch**  
+//    Explicação: Use switch para calcular operação entre dois números.  
+//    Dica: `switch` para cada operador.
+
+/* RESOLUCAO ITEM 4
+
+print("Digite n1: ");
+double n1 = double.parse(stdin.readLineSync()!);
+print("Digite qual operador + | - | * | / : ");
+String? operador = stdin.readLineSync();
+print("Digite n2: ");
+double n2 = double.parse(stdin.readLineSync()!);
+double resultado = 0;
+switch (operador) {
+  case "+":
+    resultado = n1 + n2;
+    break;
+    case "-":
+    resultado = n1 - n2;
+    break;
+    case "*":
+    resultado = n1 * n2;
+    break;
+    case "/":
+    resultado = n1 / n2;
+    break;
+}
+
+print("O Resultado da operação $n1 $operador $n2 = $resultado");
+
+*/
+
+
+// 5. **Mês do ano pelo número**  
+//    Explicação: Mostre nome do mês pelo número, use `default` para inválidos.  
+//    Dica: Use `switch`.
+
+print("Digite qual mês deseja de 1 até 12: ");
+int entradaMes = int.parse(stdin.readLineSync()!);
+switch (entradaMes) {
+  case 1:
+    print("Escolheu mês de JANEIRO");
+
+    break;
+    case 2:
+    print("Escolheu mês de FEVEREIRO");
+    
+    break;
+    case 3:
+    print("Escolheu mês de MARÇO");
+    
+    break;
+     case 4:
+    print("Escolheu mês de ABRIL");
+
+    break;
+    case 5:
+    print("Escolheu mês de MAIO");
+    
+    break;
+    case 6:
+    print("Escolheu mês de JUNHO");
+    
+    break;
+      case 7:
+    print("Escolheu mês de JULHO");
+
+    break;
+    case 8:
+    print("Escolheu mês de AGOSTO");
+    
+    break;
+    case 9:
+    print("Escolheu mês de SETEMBRO");
+    
+    break;
+     case 10:
+    print("Escolheu mês de OUTUBRO");
+
+    break;
+    case 11:
+    print("Escolheu mês de NOVEMBRO");
+    
+    break;
+    case 12:
+    print("Escolheu mês de DEZEMBRO");
+    
+    break;
+  default: 
+  print("Digitou incorretamente, recomece novamente");
+}
 
 
 }
